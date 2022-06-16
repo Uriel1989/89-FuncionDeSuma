7968b869745654c9ad8bd534d369d075f5db2b1a
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:56:45 -0300

    Add files via upload

---
+++ 89-FuncionDeSuma/Operacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _89_FuncionDeSuma
+{
+    class Operacion
+    {
+        public int suma (int primero, int segundo)
+        {
+            int suma = primero + segundo;
+            return suma;
+        }
+    }
+}
---
+++ 89-FuncionDeSuma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//Crea una función que sume dos números pasados por parámetros, devolverá el resultado.
+
+namespace _89_FuncionDeSuma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operacion mat = new Operacion();
+
+            int resultado = mat.suma(15, 35);
+
+            Console.WriteLine(resultado);
+
+
+        }
+    }
+}
